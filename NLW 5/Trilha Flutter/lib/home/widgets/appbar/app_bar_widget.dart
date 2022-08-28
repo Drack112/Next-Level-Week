45e433ef6b5a9cd5abe495a1edddb7fdfd569208
Drack <61944386+Drack112@users.noreply.github.com>
2022-08-28 14:53:43 -0300

    🚀 RELEASE: Finish NLW 5 of flutter

--- NLW 5/Trilha Flutter/lib/home/widgets/appbar/app_bar_widget.dart
+++ NLW 5/Trilha Flutter/lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,11 @@
 import 'package:DevQuiz/core/core.dart';
 import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
-
-import 'package:flutter/widgets.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
+import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+  AppBarWidget({required this.user})
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
@@ -13,42 +14,39 @@ class AppBarWidget extends PreferredSize {
               children: [
                 Container(
                   height: 161,
-                  width: double.maxFinite,
                   padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
                   decoration: BoxDecoration(gradient: AppGradients.linear),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text.rich(
-                        TextSpan(
+                      Text.rich(TextSpan(
                           text: "Olá, ",
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
-                              text: "Drack",
+                              text: user.name,
                               style: AppTextStyles.titleBold,
                             )
-                          ],
-                        ),
-                      ),
+                          ])),
                       Container(
                         width: 58,
                         height: 58,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
-                            image: NetworkImage(
-                                "https://avatars.githubusercontent.com/u/61944386?v=4"),
+                            image: NetworkImage(user.photoUrl),
                           ),
                         ),
-                      ),
+                      )
                     ],
                   ),
                 ),
                 Align(
-                  alignment: Alignment(0.0, 1.0),
-                  child: ScoreCardWidget(),
-                ),
+                    alignment: Alignment(0.0, 1.0),
+                    child: ScoreCardWidget(
+                      percent: user.score / 100,
+                    ))
               ],
             ),
           ),
--- NLW 5/Trilha Flutter/lib/main.dart
+++ NLW 5/Trilha Flutter/lib/main.dart
@@ -3,7 +3,5 @@ import 'package:flutter/material.dart';
 import 'core/app_widget.dart';
 
 void main() {
-  runApp(
-    AppWidget(),
-  );
+  runApp(AppWidget());
 }
